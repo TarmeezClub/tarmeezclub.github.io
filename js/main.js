b6abec6473181a6b12cd26032a0499b2eb93386d
Abdullah Khalid bin Ammar <62870638+A222moq3e@users.noreply.github.com>
2023-11-09 18:45:34 +0300

    add animation to scroll for us.html page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-document.querySelector('.logo').addEventListener('clikc',()=>{
+document.querySelector('.logo').addEventListener('click',()=>{
     window.location.href = 'http://google.com'
 })
 
+
+window.addEventListener('scroll',revalCards)
+function revalCards(){
+  
+    let flexboxMember = document.querySelectorAll('.flexbox-member');
+    // Scroll Listner 
+    let lastScrollTop=0;
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    flexboxMember.forEach(element => {
+        let elementTop = element.getBoundingClientRect().top;
+        if (elementTop-500<0   && !element.classList.contains('reval')) {
+        element.classList.add('reval');
+        } 
+    });
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}
